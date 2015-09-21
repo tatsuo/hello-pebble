f2ff97dfa779e9d53f78cb05c886361ae23411b3
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2015-09-21 21:53:29 +0900

    Add init & deinit

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -1,5 +1,19 @@
 #include <pebble.h>
 
+Window *window;
+
+void init() {
+  window = window_create();
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  window_destroy(window);
+}
+
 int main(void) {
+  init();
   app_event_loop();
+  deinit();
+  return 0;
 }
